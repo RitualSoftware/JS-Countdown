2f19766d90adff54fd8917c8d8c540c9d39668de
Ritual Software <72323796+RitualSoftware@users.noreply.github.com>
2021-09-26 14:24:02 +0300

    Create countdown.js

---
+++ countdown.js
@@ -0,0 +1,25 @@
+<script>
+  var displayLocation  = document.getElementById("timeLeft");
+  var elapsedTime;
+  var secconds = 1800;
+  var counter = setInterval(function () {
+            counterBegin();
+      }, 1000);
+
+function counterBegin() {
+    var minutes = Math.floor(secconds / 60),
+    secondsLeft  = secconds % 60;
+      if (secondsLeft < 10) {
+        secondsLeft = '0' + secondsLeft;
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+    displayLocation.innerHTML = minutes + ":" + secondsLeft;
+      if (secconds > 0) {
+          secconds = secconds - 1;
+      } else {
+        clearInterval(counter);
+      }
+ }
+</script>
